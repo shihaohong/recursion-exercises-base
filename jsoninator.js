1819a80e44e7f8f031d133a5536782a7e985a3ec
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-14 03:55:36 -0700

    completed recursion exercises

--- jsoninator.js
+++ jsoninator.js
@@ -5,9 +5,52 @@ const _ = require('underscore'); // the real one! :)
 // But you don't. So you're going to write it from scratch...
 
 
-
 const stringify = function(obj) {
-  // your code goes here
+  var arrOfValues = [];
+  var arrOfKeys = [];
+  var arrOfObjVal = [];
+
+  // primitive types
+  if (typeof(obj) === 'boolean' || typeof(obj) === 'number' || obj === null) {
+    return '' + obj;
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+  // arrays 
+  else if (Array.isArray(obj)) {
+    if(obj[0] === undefined) {
+      return '[]';
+    } else { 
+      obj.forEach(function(val) { // searches through every element
+        arrOfValues.push(stringify(val)); // stringifies elements, nested or not
+      });
+      return '[' + arrOfValues + ']'; // returns complete array
+    }
+  }
+
+  // objects
+  else if (obj instanceof Object) {
+    arrOfKeys = Object.keys(obj) // acquires object keys
+    arrOfKeys.forEach(function(key) {
+      var keyString = '"' + key + '":';
+      var objVal = obj[key];
+
+      // primitive types
+      if (typeof(objVal) === 'boolean' || typeof(objVal) === 'number' || obj === null) {
+        arrOfObjVal.push('' + objVal);
+      } else if (typeof(objVal) === 'string') {
+        arrOfObjVal.push(keyString + '"' + objVal + '"');
+      } 
+
+      // nested objects
+      else if (objVal instanceof Object) {
+        arrOfObjVal.push(keyString + stringify(objVal));
+      }
+    });
+
+    return '{' + arrOfObjVal + '}';
+  }
 };
 
 module.exports = {
